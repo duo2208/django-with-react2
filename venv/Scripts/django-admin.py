0a9533923ed5242ac2e445d947c6ca9c6d2653ae
jimin lee <duo2208@likelion.org>
2021-11-03 00:22:47 +0900

    inital

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\izimi\onedrive\바탕 화면\programing\vscode\django_with_react2\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
